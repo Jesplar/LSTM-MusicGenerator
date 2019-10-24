44f05951dd5a130cc3f1c26acd9cb3d7064631d9
Matilda Wikström <matilda@solutionseeker.com>
2019-10-24 16:21:48 +0200

    added code to results folder

---
+++ Results/Code/preprocessing.py
@@ -0,0 +1,101 @@
+#import tensorflow as tf
+#from tensorflow.keras import backend as K
+import glob
+import random
+import pretty_midi
+import IPython
+import numpy as np
+from tqdm import tnrange, tqdm_notebook, tqdm
+from random import shuffle, seed
+import numpy as np
+#from tensorflow.keras.losses import sparse_categorical_crossentropy
+#from tensorflow.keras.optimizers import Nadam
+import numpy as np
+from numpy.random import choice
+import pickle
+import matplotlib.pyplot as plt
+
+#Not sure if we need all these though
+
+'''
+This file preprocesses the data required for the network
+Insert all required functions
+'''
+def piano_roll_to_pretty_midi(piano_roll, fs=100, program=0):
+    '''Convert a Piano Roll array into a PrettyMidi object
+     with a single instrument.
+    Parameters
+    ----------
+    piano_roll : np.ndarray, shape=(128,frames), dtype=int
+        Piano roll of one instrument
+    fs : int
+        Sampling frequency of the columns, i.e. each column is spaced apart
+        by ``1./fs`` seconds.
+    program : int
+        The program number of the instrument.
+    Returns
+    -------
+    midi_object : pretty_midi.PrettyMIDI
+        A pretty_midi.PrettyMIDI class instance describing
+        the piano roll.
+    '''
+    notes, frames = piano_roll.shape
+    pm = pretty_midi.PrettyMIDI()
+    instrument = pretty_midi.Instrument(program=program)
+
+    # pad 1 column of zeros so we can acknowledge inital and ending events
+    piano_roll = np.pad(piano_roll, [(0, 0), (1, 1)], 'constant')
+
+    # use changes in velocities to find note on / note off events
+    velocity_changes = np.nonzero(np.diff(piano_roll).T)
+
+    # keep track on velocities and note on times
+    prev_velocities = np.zeros(notes, dtype=int)
+    note_on_time = np.zeros(notes)
+
+    for time, note in zip(*velocity_changes):
+        # use time + 1 because of padding above
+        velocity = piano_roll[note, time + 1]
+        time = time / fs
+        if velocity > 0:
+            if prev_velocities[note] == 0:
+                note_on_time[note] = time
+                prev_velocities[note] = velocity
+        else:
+            pm_note = pretty_midi.Note(
+                velocity=prev_velocities[note],
+                pitch=note,
+                start=note_on_time[note],
+                end=time)
+            instrument.notes.append(pm_note)
+            prev_velocities[note] = 0
+    pm.instruments.append(instrument)
+    return pm
+
+def get_list_midi(folder = 'maestro-v1.0.0/**/*.midi', seed_int = 666):
+  """Get the list of all midi file in the folders
+  
+  Parameters
+  ==========
+  folder : str
+    The midi folder.
+  seed_int : int
+    the random seed.
+  
+  Returns
+  =======
+  The midi files
+  
+  """
+  list_all_midi = glob.glob(folder)
+  seed(seed_int)
+  shuffle(list_all_midi)
+  return list_all_midi
+
+# list_all_midi = get_list_midi() Use this function in 
+
+
+
+
+
+
